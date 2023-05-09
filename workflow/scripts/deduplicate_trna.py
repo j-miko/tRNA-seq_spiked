be88eeaff96c9d992309b3edbe1324e209bf0789
j-miko <jan.a.mikolajczyk@gmail.com>
2023-05-09 14:00:52 +0200

    added scripts

--- workflow/scripts/deduplicate_trna.py
+++ workflow/scripts/deduplicate_trna.py
@@ -11,7 +11,7 @@ df00 = df00.rename({0:'gene', 1:'seq'}, axis=1)
 df00['seq'] = [x.replace('U', 'T') for x in df00['seq'].values]
 df00['gene'] = df00['gene'].str.split(' ', expand=True)[0]
 ss = df00['gene'].str.split("-", expand=True)
-species_prefix = ss[0]
+species_prefix = ss[0][0]
 df00['anticodon'] = ss.apply(lambda x: '-'.join([x[1], x[2]]), axis=1)
 df00['isodecoder'] = ss.apply(lambda x: '-'.join([x[3], x[4]]), axis=1)
 # merge duplicate isodecoders into single records
