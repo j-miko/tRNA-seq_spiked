8cecd6fa9bcefb6b649c9b220d55e0ab23447630
j-miko <jan.a.mikolajczyk@gmail.com>
2023-05-09 11:04:01 +0200

    modified workflow for spike-in

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# tRNA-seq_spiked
-Pipeline for analysis of data from tRNA-seq experiments with spike-ins
+This is a workflow for processing and quanitifying tRNA-seq reads.
+
+Setup:
+
+1. Put files containing raw reads in FASTQ format in the 'fastq' directory. Make sure their extensions are '.fastq.gz', as all other types will be ignored.
+2. Put correct bowtie2 index files in the 'references/bowtie2_index' directory.
+3. Put correct salmon index folder in the 'references/salmon_index' directory.
+4. Put "xxxx-mature-tRNAs.fa" file form gtRNA-db in the 'tRNA' directory
+5. In the snakefile, set:
+  - BOWTIE_INDEX to 'references/bowtie2_index/{name_of_your_bowtie2_index}' – don't include file extensions, just the name of your index
+  - SALMON_INDEX to 'references/salmon_index/{name_of_your_salmon_index}' – again, just the folder name
+  - TRNA_FASTA to '/references/trna/{your_mature-trna.fa}'
+
+Running pipeline:
+
+First run:
+```
+snakemake -n
+```
+
+to verify if you've set everything up properly. If it doesn't throw up any errors, you're good to go.
+
+To run full pipeline:
+```
+snakemake -p --use-conda --cores
+```
---
+++ workflow/scripts/deduplicate_trna.py
@@ -0,0 +1,25 @@
+import sys
+import pandas as pd
+
+# Usage: deduplicate_trna.py <input file> <output filename>
+
+# input mature trna fastsa file (after fasta2tab) from stdin
+trna_file = sys.argv[1]
+out_filename = sys.argv[2]
+df00 = pd.read_csv(trna_file, sep='\t', header=None)
+df00 = df00.rename({0:'gene', 1:'seq'}, axis=1)
+df00['seq'] = [x.replace('U', 'T') for x in df00['seq'].values]
+df00['gene'] = df00['gene'].str.split(' ', expand=True)[0]
+ss = df00['gene'].str.split("-", expand=True)
+species_prefix = ss[0]
+df00['anticodon'] = ss.apply(lambda x: '-'.join([x[1], x[2]]), axis=1)
+df00['isodecoder'] = ss.apply(lambda x: '-'.join([x[3], x[4]]), axis=1)
+# merge duplicate isodecoders into single records
+df01 = df00.groupby('seq')['isodecoder'].apply(lambda x: ';'.join(x)).reset_index()
+df01 = df01.merge(df00[['seq','anticodon']].drop_duplicates(), on='seq',how='left')
+df01['gene'] = df01.apply(lambda x: '_'.join([species_prefix, x['anticodon'], x['isodecoder']]),axis=1)
+df01['len'] = df01['seq'].apply(len)
+with open(out_filename, 'w') as f:
+    for row in range(df01.shape[0]):
+        f.write(">" + df01['gene'][row] + " " + str(df01['len'][row]) + '\n')
+        f.write(df01['seq'][row] + '\n')
